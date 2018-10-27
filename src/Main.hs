5958d95260857a391bfb411d54337fbfd90ea6fb
bennie010697 <bennie.veldhuijzen@gmail.com>
2018-10-27 13:47:46 +0200

    k

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,16 @@
 module Main where
 
-import Controller (step, input)
-import Model (initialState)
-import View (view)
+import Controller
+import Model
+import View
 
-import System.Environment (getArgs, getProgName)
 import Graphics.Gloss.Interface.IO.Game
-import Graphics.Gloss.Data.Display (Display(..))
 
 main :: IO ()
-main = do
-    args <- getArgs
-    prog <- getProgName
-    case args of
-        ["Windowed"]    -> putStrLn "Running in Windowed mode" >> main' (InWindow "PagChomp" (400, 400) (0, 0)) -- size , position
-        ["FullScreen"]  -> putStrLn "Running in FullScreen mode" >> main' FullScreen
-        _               -> putStrLn $ "USAGE: " ++ prog ++ " [Windiwed|FullScreen]"
-
-
-main' :: Display -> IO ()
-main' dis = playIO dis
-    black               -- Background color
-    30                  -- Frames per second
-    (initialState dis)  -- Initial state
-    view                -- View function
-    input               -- Event function
-    step                -- Step function
+main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
+              black            -- Background color
+              10               -- Frames per second
+              initialState     -- Initial state
+              view             -- View function
+              input            -- Event function
+              step             -- Step function
--- src/Model.hs
+++ src/Model.hs
@@ -2,50 +2,17 @@
 --   which represent the state of the game
 module Model where
 
-import Graphics.Gloss.Data.Display (Display(..))
-import Graphics.Gloss.Data.Point (Point)
+data InfoToShow = ShowNothing
+                | ShowANumber Int
+                | ShowAChar   Char
 
-newtype Row a = Row [a]
-type Column a = Row a
-data Grid a = Grid [Row a] [Column a] | Grid_1D [Row a] deriving Show
+nO_SECS_BETWEEN_CYCLES :: Float
+nO_SECS_BETWEEN_CYCLES = 5
 
-data Portal = Ref Portal | Val Field
-data Field = Empty
-            | Wall
-            | Portal 
-            | Player
-            | Ghost
-            | Fruit
-            deriving (Show)
-          
-data GameField = GameField {
-                  grid :: Grid Field
-                , upLeft :: Point
-                , downRight :: Point
-}
 data GameState = GameState {
-                   gameField :: GameField
+                   infoToShow  :: InfoToShow
                  , elapsedTime :: Float
-                 , windowType :: Display
                  }
 
-instance Show a => Show (Row a) where
- show (Row as) = concat $ map (\x -> show x ++ " ") as
-                
-
-createGrid :: Show a => a -> Int -> Int -> Grid a
-createGrid initVal width height = let rep w h = map Row . replicate w . replicate h 
-                                  in Grid (rep height width initVal) (rep width height initVal)
-
-prettyPrintGrid :: Show a => Grid a -> IO()
-prettyPrintGrid (Grid rows columns) = let print = mapM_ putStrLn . map (\x -> "|" ++ show x ++ "|")
-                                      in putStrLn "Rows" >> print rows >> putStrLn "Columns"  >> print columns
-
-templateWindowed :: Display
-templateWindowed = (InWindow "template" (0,0) (0,0))
-
-initialFieldState :: Grid Field
-initialFieldState = createGrid Empty 10 10
-
-initialState :: (Display -> GameState)
-initialState = GameState (GameField initialFieldState (200,200) (-200,-200)) 0
+initialState :: GameState
+initialState = GameState ShowNothing 0
