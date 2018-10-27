18302ed6057b0f108be17d37b06484b6bdcc089a
bennie010697 <bennie.veldhuijzen@gmail.com>
2018-10-27 13:51:32 +0200

    Revert "k"
    This reverts commit 5958d95260857a391bfb411d54337fbfd90ea6fb.

--- src/Controller.hs
+++ src/Controller.hs
@@ -2,30 +2,48 @@
 --   in response to time and user input
 module Controller where
 
-import Model
+import Model(prettyPrintGrid, templateWindowed, GameState(..))
+import Graphics.Gloss.Data.Display (Display(..))
 
+
+--import Graphics.Rendering.OpenGL
+
+import Graphics.UI.GLUT.Window(leaveFullScreen, fullScreen, windowTitle)
+--import Graphics.UI.GLUT.Window (leaveFullScreen)
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Data.Display (Display(..))
 import System.Random
+import Data.StateVar
 
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
-step secs gstate
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
+step secs gstate@(GameState field _ _) = return gstate
+
+{-
+  | (round $ elapsedTime gstate + secs) `mod` 60 == 0
   = -- We show a new random number
-    do randomNumber <- randomIO
-       let newNumber = abs randomNumber `mod` 10
-       return $ GameState (ShowANumber newNumber) 0
+    prettyPrintGrid field >> (return $ gstate { elapsedTime = elapsedTime gstate + secs } )
   | otherwise
   = -- Just update the elapsed time
     return $ gstate { elapsedTime = elapsedTime gstate + secs }
-
+-}
 -- | Handle user input
 input :: Event -> GameState -> IO GameState
-input e gstate = return (inputKey e gstate)
+input e gstate = inputKey e gstate
 
-inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) _ _ _) gstate
-  = -- If the user presses a character key, show that one
-    gstate { infoToShow = ShowAChar c }
-inputKey _ gstate = gstate -- Otherwise keep the same
+inputKey :: Event -> GameState -> IO GameState
+inputKey (EventKey (SpecialKey KeyEsc) _ _ _) gstate = handleESC gstate
+inputKey (EventKey (SpecialKey KeyF5) _ _ _) gstate  = handleF5 gstate
+inputKey _ gstate = return gstate -- Otherwise keep the same
+
+handleESC :: GameState -> IO GameState
+handleESC gstate@(GameState _ _ FullScreen) = leaveFullScreen >> 
+                                                (windowTitle $= "PogChamp")  >>
+                                                (return $ gstate {windowType = (templateWindowed)})
+handleESC gstate                              = return gstate
+
+handleF5 :: GameState -> IO GameState
+handleF5 gstate@(GameState _ _ (InWindow _ _ _ ))  = fullScreen >>
+                                                      (return $ gstate {windowType = FullScreen})
+handleF5 gstate                                     = return gstate
--- src/View.hs
+++ src/View.hs
@@ -2,6 +2,8 @@
 --   the game state into a picture
 module View where
 
+import Data.Monoid((<>))
+
 import Graphics.Gloss
 import Model
 
@@ -9,7 +11,20 @@ view :: GameState -> IO Picture
 view = return . viewPure
 
 viewPure :: GameState -> Picture
-viewPure gstate = case infoToShow gstate of
-  ShowNothing   -> blank
-  ShowANumber n -> color green (text (show n))
-  ShowAChar   c -> color green (text [c])
+viewPure gstate@(GameState gf@(GameField field _ _) time _) = case field of
+  ( Grid [Row [Empty]] [Row [Empty]] ) -> viewField gf
+  otherwise -> viewField gf
+  
+viewField :: GameField -> Picture
+viewField (GameField (Grid rows columns) upL downR) = Color green $ viewGrid upL downR (length rows) (length columns)
+
+viewGrid :: Point -> Point -> Int -> Int -> Picture
+viewGrid upLeft@(u,v) downRight@(z,w) cellsH cellsV = linesHor <> linesVert
+  where linesHor   = foldr (\x -> mappend (Line x) ) blank [ [ (u, y), (z, y) ] | y <- [v, v - yStep .. w] ]
+        linesVert  = foldr (\x -> mappend (Line x) ) blank [ [ (x, v), (x, w) ] | x <- [u, u - xStep .. z] ]
+        xStep = (u - z) / (fromIntegral cellsH)
+        yStep = (v - w) / (fromIntegral cellsV)
+{-
+  case elapsedTime gstate of
+  otherwise -> color green (text "hey")
+-}
